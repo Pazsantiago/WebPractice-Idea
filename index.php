2914b2a91206cf48ce278cc0f348cf6c7ea103c8
Santiago <santiago.ariel.paz04@gmail.com>
2021-01-11 20:22:26 -0300

    Bases del headers y sections

--- index.php
+++ index.php
@@ -4,11 +4,44 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="css/styles.css">
+	<script src="https://kit.fontawesome.com/0030d3af8f.js" crossorigin="anonymous"></script>
 	<title>Pagina web</title>
 </head>
 <body>
 	<main class="main-container">
-		
+
+		<div class="container-headersection">
+			<header class="header">
+			
+			<img src="" alt="">
+
+			<nav>
+				<ul>
+					<li class="nav-container__link">Home <i class="fas fa-caret-down"></i></li>
+					<li class="nav-container__link">Pages <i class="fas fa-caret-down"></i></li>
+					<li class="nav-container__link">Portfolio <i class="fas fa-caret-down"></i></li>
+					<li class="nav-container__link">Features <i class="fas fa-caret-down"></i></li>
+					<li class="nav-container__link">Demos <i class="fas fa-caret-down"></i></li>
+					<li class="nav-container__link">Shop <i class="fas fa-caret-down"></i></li>
+					<li class="nav-container__link">Blog <i class="fas fa-caret-down"></i></li>
+				</ul>
+				<i class="fas fa-search"></i>
+			</nav>
+
+			</header>
+
+			<section>
+				
+			</section>
+
+		</div>
+
+		<section>
+			
+
+		</section>
+
 	</main>
+
 </body>
 </html>
